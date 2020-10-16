aae7f9afc50f0a8bebe60b16cc2717306a3e5a9a
Anderson-Chiang <cachiang814@gmail.com>
2020-10-16 18:16:53 +0800

    feat: add sort function

--- app.js
+++ app.js
@@ -34,6 +34,7 @@ app.get('/', (req, res) => {
   // 拿到全部的 Todo 資料
   Todo.find()
     .lean()
+    .sort({ _id: 'asc' }) // (desc 降冪)，_id 是按照 mongo DB 生成的順序來排序
     .then(todos => res.render('index', { todos: todos }))
     .catch(error => console.error(error))
 })
