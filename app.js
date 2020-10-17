e9c8a1b9569187eecb3d52c570eb0a31b721cd20
Anderson-Chiang <cachiang814@gmail.com>
2020-10-17 15:29:53 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,28 +1,13 @@
 const express = require('express')
-const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 
-const Todo = require('./models/todo') // 相對路徑 (./)
-
 const routes = require('./routes') //可以不用寫/index，因為node.js預設會去找
+require('./config/mongoose') //不用設變數來接
+
 const app = express()
 
-// connect database (url解析的東西 & 監控的引擎被棄用，兩個都需要加入參數-可複製貼上)
-mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true })
-
-// 把 connect 的東西暫存下來
-const db = mongoose.connection
-
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-
-// 用 once 是因為連線成功一次即可，做完一次就會自動把它拿掉
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
 
 // set template engine
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
---
+++ config/mongoose.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+
+// connect database (url解析的東西 & 監控的引擎被棄用，兩個都需要加入參數-可複製貼上)
+mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true })
+
+// 把 connect 的東西暫存下來
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+// 用 once 是因為連線成功一次即可，做完一次就會自動把它拿掉
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
+module.exports = db
